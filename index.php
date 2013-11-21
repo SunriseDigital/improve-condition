957d33f7552638d5ebd5031b717012a422a8d6c7
atsushi watanabe <watanabe@sunrisedigital.jp>
2013-11-21 13:38:48 +0900

    サンライズのリポジトリに移行。

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="jp">
+<head>
+	<meta charset="UTF-8">
+	<title>improve-condition</title>
+	<link rel="stylesheet" href="/improve-condition/css/index.css" type="text/css" media="all">
+</head>
+<body>
+
+</body>
+</html>
