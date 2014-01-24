946bbf3cd1ee12e7c6b25394aa00b66006334c7a
sunrise-sakurai <sakurai@sunrisedigital.jp>
2014-01-24 12:39:29 +0900

    キャッシュを取っていたので設定変更

--- sakurai/config.rb
+++ sakurai/config.rb
@@ -1,6 +1,9 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
+cache = false
+asset_cache_buster :none
+
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
@@ -8,7 +11,6 @@ images_dir = "img"
 javascripts_dir = "js"
 output_style = :expanded
 line_comments = false
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
