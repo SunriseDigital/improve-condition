c99d6055db9b117b850bf0f0694e289e228b92c5
sunrise-sakurai <sakurai@sunrisedigital.jp>
2014-04-08 13:21:31 +0900

    モジュールを考えつつコーディング

--- sakurai/index.php
+++ sakurai/index.php
@@ -12,255 +12,275 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	</head>
 	<body>
-		<div class="wrapper">
-			<!-- ヘッダー -->
-			<div class="header">
+		<!-- ヘッダー -->
+		<div class="header">
+			<div class="header-wrap">
 				<h1 class="">caption</h1>
 			</div>
+		</div>
 
-			<!-- ナビゲーション -->
-			<div class="primaryNav">
-				<ul class="">
-					<li class="">nav</li>
-					<li class="">nav</li>
-					<li class="">nav</li>
-					<li class="">nav</li>
-					<li class="">nav</li>
-				</ul>
-			</div>
+		<!-- ナビゲーション -->
+		<div class="primaryNav">
+			<ul class="primaryNav-wrap">
+				<li class="">nav</li>
+				<li class="">nav</li>
+				<li class="">nav</li>
+				<li class="">nav</li>
+				<li class="">nav</li>
+			</ul>
+		</div>
 
-			<!-- メインカラム -->
-			<div class="mainContainer">
-				<div class="mainColumn">
-					<!-- 画像スライダー -->
-					<!-- <div class="">
-						<ul class="">
-							<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
-							<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
-							<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
-							<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
-							<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
-						</ul>
-						<ul class="">
-							<li class="">marker</li>
-							<li class="">marker</li>
-							<li class="">marker</li>
-							<li class="">marker</li>
-							<li class="">marker</li>
-						</ul>
-					</div> -->
-					<div class="j-slider">
-						<div><img src=""></div>
-					</div>
-
-					<!-- メインコンテンツ (ラベルとサムネイルが有るリスト) -->
-					<div class="">
-						<ul class="">
-							<li class="parallelList2">
-								<p class="label--health">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2--flap">
-								<p class="label--love">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2">
-								<p class="label--fresh">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2--flap">
-								<p class="label--fresh">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2">
-								<p class="label--health">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2--flap">
-								<p class="label--love">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2">
-								<p class="label--love">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2--flap">
-								<p class="label--fresh">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2">
-								<p class="label--love">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-							<li class="parallelList2--flap">
-								<p class="label--health">label</p>
-								<p class="parallelList2-text">paragraph</p>
-								<img src="" alt="" class="parallelList2-thumb">
-							</li>
-						</ul>
-					</div>
-
-					<!-- 3枠並びリスト -->
-					<div class="">
-						<ul class="">
-							<li class="parallelList3">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="parallelList3">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="parallelList3--flap">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="parallelList3">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="parallelList3">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="parallelList3--flap">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-						</ul>
-					</div>
-
-					<!-- メインコンテンツ　(画像だけのリスト) -->
-					<div class="">
-						<h2 class=""></h2>
-						<ul class="">
-							<li class="">
-								<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
-							</li>
-							<li class="">
-								<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
-							</li>
-							<li class="">
-								<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
-							</li>
-							<li class="">
-								<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
-							</li>
-							<li class="">
-								<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
-							</li>
-						</ul>
-						<p>to list</p>
-					</div>
-
-					<!-- メインコンテンツ　(段落と画像だけのリスト) -->
-					<div class="">
-						<ul class="">
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-							<li class="">
-								<p class="">paragraph</p>
-								<img src="" alt="">
-							</li>
-						</ul>
-					</div>
-
-					<!-- メインコンテンツ (テキストだけのリスト) -->
-					<div class="">
-						<ul class="">
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-							<li class="list">text</li>
-						</ul>
-					</div>
-
+		<!-- メインカラム -->
+		<div class="mainContainer">
+			<div class="mainColumn">
+				<!-- 画像スライダー -->
+				<!-- <div class="">
+					<ul class="">
+						<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
+						<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
+						<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
+						<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
+						<li class=""><img src="http://dummyimage.com/650x330/000/fff" alt=""></li>
+					</ul>
+					<ul class="">
+						<li class="">marker</li>
+						<li class="">marker</li>
+						<li class="">marker</li>
+						<li class="">marker</li>
+						<li class="">marker</li>
+					</ul>
+				</div> -->
+				<div class="j-slider">
+					<div><img src="img/bigimage.gif"></div>
 				</div>
 
-				<!-- レフトカラム -->
-				<div class="subColumn">
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
-					<!-- サブコンテンツ -->
-					<div class=""></div>
-
+				<!-- メインコンテンツ (ラベルとサムネイルが有るリスト) -->
+				<div class="">
+					<ul class="">
+						<li class="parallelList2">
+							<div class="parallelList2-text">
+								<p class="label--health">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>									
+							</div><!-- 
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<div class="parallelList2-text">
+								<p class="label--love">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<div class="parallelList2-text">
+								<p class="label--fresh">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>	
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<div class="parallelList2-text">
+								<p class="label--fresh">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<div class="parallelList2-text">
+								<p class="label--health">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<div class="parallelList2-text">
+								<p class="label--love">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>									
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<div class="parallelList2-text">
+								<p class="label--love">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<div class="parallelList2-text">
+								<p class="label--fresh">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<div class="parallelList2-text">
+								<p class="label--love">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>									
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<div class="parallelList2-text">
+								<p class="label--health">label</p>
+								<p><a class="listLinkTitle" href="">paragraph</a></p>									
+							</div><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li>
+					</ul>
 				</div>
+
+				<!-- 3枠並びリスト -->
+				<div class="">
+					<ul class="">
+						<li class="parallelList3">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList3">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList3--flap">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList3">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList3">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList3--flap">
+							<p class="parallelList3-text"><a href="">paragraph</a></p><!--
+							--><a href="" class="parallelList3-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li>
+					</ul>
+				</div>
+
+				<!-- メインコンテンツ　(画像だけのリスト) -->
+				<div class="">
+					<h2 class=""></h2>
+					<ul class="">
+						<li class="">
+							<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
+						</li>
+						<li class="">
+							<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
+						</li>
+						<li class="">
+							<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
+						</li>
+						<li class="">
+							<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
+						</li>
+						<li class="">
+							<img src="http://dummyimage.com/105x160/000/fff" alt="dummyimage">
+						</li>
+					</ul>
+					<p>to list</p>
+				</div>
+
+				<!-- メインコンテンツ　(段落と画像だけのリスト) -->
+				<div class="">
+					<ul class="">
+						<li class="parallelList2">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li><!--
+						--><li class="parallelList2--flap">
+							<p class="parallelList2-text"><a class="listLinkTitle" href="">paragraph</a></p><!--
+							--><a href="" class="parallelList2-thumb"><img src="img/sample.jpg" alt=""></a>
+						</li>
+					</ul>
+				</div>
+
+				<!-- メインコンテンツ (テキストだけのリスト) -->
+				<div class="">
+					<ul class="">
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+						<li class="list"><a href="">text</a></li>
+					</ul>
+				</div>
+
 			</div>
 
-			<!-- フッター -->
-			<div class="footer">
-				<ul class="">
-					<li class="">something</li>
-					<li class="">something</li>
-					<li class="">something</li>
-					<li class="">something</li>
-					<li class="">something</li>
-				</ul>
-				<p><small>&copy; something</small></p>
+			<!-- レフトカラム -->
+			<div class="subColumn">
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
+				<!-- サブコンテンツ -->
+				<div class="">banner</div>
+
 			</div>
+		</div>
+
+		<!-- フッター -->
+		<div class="footer">
+			<ul class="">
+				<li class="">something</li>
+				<li class="">something</li>
+				<li class="">something</li>
+				<li class="">something</li>
+				<li class="">something</li>
+			</ul>
+			<p><small>&copy; something</small></p>
 		</div>
 	</body>
 </html>
