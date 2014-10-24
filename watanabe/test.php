44e4bccf60e3e89a302436fcbbfa1a47f3420fa1
atsushi watanabe <watanabe@sunrisedigital.jp>
2014-10-24 17:53:14 +0900

    add test.php

---
+++ watanabe/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>中間レイヤー作業用</title>
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<link rel="stylesheet" href="/improve-condition/watanabe/css/index.css" media="all">
+		<!-- link rel="alternate" media="only screen and (max-width: 640px)" href="" / -->
+		<!-- link rel="alternate" media="handheld" href="" / -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	</head>
+	<body>
+
+	<p>master side</p>
+	<p>fork side</p>
+	<p></p>
+
+	</body>
+</html>
